e3929e45537926a0e522ef71da79cf9b5f014160
sergei_arkhipov_1 <byrgyin@yandex.ru>
2025-06-11 12:57:05 +0300

    begin_start_learn_nuxt_js

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const some:string = 'info';
+</script>
+<template>
+    <h1>Hello world!</h1>
+    <p>{{some}}</p>
+</template>
+
+<style scoped lang="css">
+h1 {
+  color: red;
+}
+</style>
